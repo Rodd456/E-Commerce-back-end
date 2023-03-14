dfb9c37781dd1ec34dd6724e1751ce8e57279ee1
Rodney Major <rodneyjr2001@gmail.com>
2023-03-14 02:23:38 -0400

    Ecom

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primalKey: true,
+      autoIncrement: true,
+      allowNull: false
+
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,21 +5,62 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoryData = awaitCategory.findAll({
+      // be sure to include its associated products
+      include: {
+        model: Product,
+        attributions: ['id', 'product_name' , 'price', 'category_id'],
+      }
+  });
+  return res.status(200).json(categoryData);
+}
+catch(err){
+  res.status(404).json
+}
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  try {
+    const categoryData = awaitCategory.findByPk(req.params.id, {
+      include: {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+       }
+    });
+    return res.status(200).json(categoryData);
+  }catch(err){
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
-});
+  try {
+    const categoryData = awaitCategory.create(req.body); 
+      return res.status(200).json(categoryData);
+       }catch (err){
+        res.status(400).json(err);
+       }
+    });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+    router.put('/:id', async (req, res) => {
+      // update a category by its `id` value
+      try {
+        const categoryData = await Category.update(req.body, {
+          where: {
+            id: req.params.id,
+          },
+          individualHooks: true 
+        });
+        res.status(200).json(categoryData); 
+      } catch (err) { 
+        res.status(500).json(err); e
+      }
+    });
+    
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
